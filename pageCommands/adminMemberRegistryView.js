730d42230c32b883e3a477b2ef9df5972a596ccf
Prem Raghvani <69864865+premraghvani@users.noreply.github.com>
2025-08-18 20:31:12 +0100

    basic garderning; renaming files, adding filler of stuff i dont intend on doing soon

---
+++ pageCommands/adminMemberRegistryView.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+// main
+module.exports = {
+    name: "GET/admin/registry",
+    description: "Admin way to add members",
+    execute: async(event, verification) => {
+        // accesss
+        if(verification.privilege != "admin"){
+            const forbiddenPage = require("./error403");
+            return await forbiddenPage.execute(event,verification)
+        }
+
+        let content = `<p>Sorry, you must ask the webmaster to do so manually as this is still a work in progress - to edit information, or as an override to add someone new. <a href="/exec">Click here</a> to go back.</p>`
+
+        // sending it to the user
+        let resp = fs.readFileSync("./assets/html/generalPage.html").toString()
+            .replace(/{{pageNameShort}}/g, "Member Registry")
+            .replace(/{{pageName}}/g, "Member Registry")
+            .replace(/{{pageDescriptor}}/g, "")
+            .replace(/{{content}}/g,  content)
+
+        return{
+            body:resp,
+            headers:{"Content-Type":"text/html"}
+        }
+    }
+}
